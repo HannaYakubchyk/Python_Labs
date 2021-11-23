b7b436c1b20d9ce12a1ea9f8383fe9f8b19e90db
HannaYakubchyk <90506113+HannaYakubchyk@users.noreply.github.com>
2021-11-23 23:33:10 +0300

    Add files via upload

---
+++ Task1.1-16.py
@@ -0,0 +1,20 @@
+a = int(input("Введите целое число от 0 до 7: "))
+
+if a == 0:
+    print("ноль")
+elif a == 1:
+    print("один")
+elif a == 2:
+    print("два")
+elif a == 3:
+    print("три")
+elif a == 4:
+    print("четыре")
+elif a == 5:
+    print("пять")
+elif a == 6:
+    print("шесть")
+elif a == 7:
+    print("семь")
+else:
+    print("Вы введили число вне диапазона")
